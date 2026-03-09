dcc083a38b240e32308d70ed6152f12868f69f92
foiehf <ahmed.nig7@gmail.com>
2026-03-09 20:35:34 +0400

    added income cacheing

--- lib/features/expenses/presentation/controllers/income_controller.dart
+++ lib/features/expenses/presentation/controllers/income_controller.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 final incomeProvider = NotifierProvider<IncomeController,double>(
   IncomeController.new
@@ -8,11 +9,20 @@ class IncomeController extends Notifier<double>{
   
   @override
   double build() {
+    getIncomeFromCache();
     return 0;
   }
 
-  void setIncome (double value){
+  void setIncome (double value)async{
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
   state = value;
+  await prefs.setDouble('income', value);
+  }
+
+  void getIncomeFromCache()async{
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    state = prefs.getDouble('income')?? 0 ;
+
   }
 
  double calculateMoneyLeft (double expenses){
